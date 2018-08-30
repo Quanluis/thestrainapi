12dc600d6f1c94ac8a95182dd99a9bbbcf8a4123
Quanluis <quanluis@protonmail.com>
2018-08-30 13:20:38 -0400

    Created a simple nav bar component and added some styles. Working with state rendering as well.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import Nav from './Nav';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    title: 'The Strain API'
+  }
+
   render() {
     return (
       <div>
-        <h1>The Strain api</h1>
+                <Nav />
+                <br/>
+        <h1>{this.state.title}</h1>
         <img className = "Cannabis" src={require('./images/Cannabis.svg')} alt=""/>
       </div>
     );
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+let Nav = () => {
+ 
+return (
+  <div>
+  <ul><a href="./Home">Home</a><a href = "./About" > About</a><a href = "./Donate">Donate</a></ul>
+  </div>
+)
+
+}
+
+export default Nav;
